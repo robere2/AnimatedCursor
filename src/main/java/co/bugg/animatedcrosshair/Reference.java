29952f1ed0d628ed5691021010a1a4182ae151f3
bugfroggy <bugfroggy@gmail.com>
2017-11-15 17:02:52 -0500

    added accepted versions limitations

--- src/main/java/co/bugg/animatedcrosshair/Reference.java
+++ src/main/java/co/bugg/animatedcrosshair/Reference.java
@@ -4,4 +4,4 @@ public class Reference {
     public static final String MOD_ID = "animatedcrosshair";
     public static final String MOD_NAME = "Animated Crosshair";
     public static final String VERSION = "1.0";
-}
+    public static final String ACCEPTED_VERSIONS = "[1.9, 1.11.2]";}
