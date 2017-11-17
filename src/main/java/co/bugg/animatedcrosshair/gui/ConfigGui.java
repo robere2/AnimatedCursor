3cccd817dfa1ca7b4d5236b10267323a9d94be84
bugfroggy <bugfroggy@gmail.com>
2017-11-17 00:39:20 -0500

    Fixed a bug with the crosshair displaying incorrect properties when ConfigGui is opened with a specified name

--- src/main/java/co/bugg/animatedcrosshair/gui/ConfigGui.java
+++ src/main/java/co/bugg/animatedcrosshair/gui/ConfigGui.java
@@ -64,6 +64,7 @@ public class ConfigGui extends GuiScreen {
     public ConfigGui(String name) {
         this();
         this.name = name.toUpperCase();
+        resetProperties();
     }
 
     @Override
